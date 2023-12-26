15d616855439ef89a44c981e57ad26be44305847
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-12-26 23:08:46 +0300

    Update routes/web.php
    - Add special route for a different dashboard for the regular user

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ Route::get('/', function () {
     ]);
 });
 
+// Dashboard Route for admin user
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
@@ -33,3 +34,9 @@ Route::middleware([
         return Inertia::render('Dashboard');
     })->name('dashboard');
 });
+
+// Special Dashboard Route for role_id 11 normal user
+Route::get('/user-dashboard', function () {
+    return Inertia::render('UserDashboard');
+})->name('user-dashboard');
+
