a58c3fec1bc89a624de1097f5009929bf68a24f1
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-12-26 23:14:19 +0300

    Create resources/js/Components/WelcomeUser.vue
    - The welcome page for the regular user

---
+++ resources/js/Components/WelcomeUser.vue
@@ -0,0 +1,23 @@
+<script setup>
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+</script>
+
+<template>
+    <div>
+        <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
+            <ApplicationLogo class="block h-12 w-auto" />
+
+            <h1 class="mt-8 text-2xl font-medium text-gray-900">
+                Welcome User
+            </h1>
+
+        </div>
+
+        <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 p-6 lg:p-8">
+            <div>
+                Welcome to your dashboard
+            </div>
+
+        </div>
+    </div>
+</template>
