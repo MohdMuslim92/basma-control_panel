7f489fb41075801216bed05b5664b16be1168e39
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-12-28 00:13:12 +0300

    Create Model Certificate

---
+++ app/Models/Certificate.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Certificate extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'admin_id',
+        'name',
+        'language',
+        'code',
+        'status',
+        'approve1',
+        'approve2',
+        'approve3',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function admin()
+    {
+        return $this->belongsTo(User::class, 'admin_id');
+    }
+}
