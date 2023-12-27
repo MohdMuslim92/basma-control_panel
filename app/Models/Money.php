2949abf4cb4516710f710a739913dacebe2faa31
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-12-28 00:13:56 +0300

    Create Model Money

---
+++ app/Models/Money.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Money extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'amount',
+        'type',
+        'admin_id',
+    ];
+
+    // Relationships
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function admin()
+    {
+        return $this->belongsTo(User::class, 'admin_id');
+    }
+}
