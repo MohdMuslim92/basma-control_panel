0faa2290772269e5810db930e94755d672e20ed5
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-12-31 16:43:09 +0300

    Add last seen field to the users table
    - The last seen field used to track users login

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -41,6 +41,7 @@ return new class extends Migration
             $table->unsignedBigInteger('status')->default(0);
             $table->string('admin_mail')->nullable();
             $table->timestamp('last_pay')->nullable();
+            $table->timestamp('last_seen_at')->nullable();
             $table->timestamps();
 
             $table->foreign('role_id')->references('id')->on('roles');
