cfae085f6537b78d6f441d92e4114801bde3d21b
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-12-31 16:46:53 +0300

    Create Middleware for UpdateLastSeen
    - Update the user last seen field whenever a user make a request through routes within the web

---
+++ app/Http/Middleware/UpdateLastSeen.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class UpdateLastSeen
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check()) {
+            // Update last seen timestamp for authenticated user
+            Auth::user()->update(['last_seen_at' => now()]);
+        }
+
+        return $next($request);
+    }
+}
