75b1281d841549b85849b80e0592574d355aa03f
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-04-27 04:38:06 +0300

    Feat: Add UserStatusSeeder
    Created a seeder (`UserStatusSeeder`) to populate
    the `user_statuses` table with initial user status
    data. The seeder inserts various user status names
    such as 'Pending', 'Active', 'Suspended', etc., into
    the database.

---
+++ database/seeders/UserStatusSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\UserStatus;
+
+class UserStatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Define an array of status names
+        $statusNames = [
+            'Pending',
+            'Active',
+            'Suspended',
+            'Inactive',
+            'Deleted',
+            'Office Member',
+            'Admin',
+            'Super Admin'
+        ];
+
+        // Loop through the status names and insert them into the statuses table
+        foreach ($statusNames as $name) {
+            UserStatus::create(['name' => $name]);
+        }
+    }
+}
