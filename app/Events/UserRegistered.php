5f82059aede3a66cf4e31ed49d9a332be0f1b2a7
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-05-05 18:42:07 +0300

    Feat: Add UserRegistered event
    This commit adds a new event class `UserRegistered`
    in the `app/Events` directory. The event is
    dispatched upon user registration and includes the
    newly registered user. It implements the
    `ShouldBroadcast` interface to enable broadcasting
    over channels.

---
+++ app/Events/UserRegistered.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use App\Models\User;
+
+class UserRegistered implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $user;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('user.' . $this->user->role_id);
+    }
+
+    public function broadcastAs()
+    {
+        return 'user.registered';
+    }
+
+}
