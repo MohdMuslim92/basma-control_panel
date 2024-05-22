ba602c8f31ccfc8e2c7d473759f6a028dc3741ec
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-05-22 03:13:09 +0300

    Refactor PaymentModal to use formatDate from Functions.js
    - Removed the formatDate function from PaymentModal and imported it from Functions.js for reusability.

--- resources/js/Pages/PaymentModal.vue
+++ resources/js/Pages/PaymentModal.vue
@@ -58,7 +58,8 @@
   <script setup>
   import { defineProps, getCurrentInstance, ref } from 'vue';
   import axios from 'axios';
-  
+  import  { formatDate } from '../Functions.js';
+
   // Get the current component instance
   const instance = getCurrentInstance();
   
@@ -73,17 +74,7 @@
   
   // Reactive variable for the selected payment type
   const selectedPaymentType = ref('Cash'); // Default payment type
-  
-  // Function to format the Last Pay date as YYYY-MM-DD
-  const formatDate = (dateString) => {
-    if (!dateString) return 'User has never paid before'; // Display message if date is null or undefined
-    const date = new Date(dateString);
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    return `${year}-${month}-${day}`;
-  };
-  
+    
   // Function to calculate the payment amount based on the selected number of months and monthly share
   const calculateAmount = () => {
     const paymentAmount = selectedMonths.value * props.monthlyShare;
