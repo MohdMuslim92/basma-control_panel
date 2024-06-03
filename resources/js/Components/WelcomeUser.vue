2c1f467c09e189f6e7bd6776bb9ef0d83e01e082
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-06-03 21:20:34 +0300

    Feat: implement translations for other components
    - Added translations to ConfirmsPassword.vue
    - Added translations to WelcomeUser.vue

--- resources/js/Components/WelcomeUser.vue
+++ resources/js/Components/WelcomeUser.vue
@@ -1,5 +1,9 @@
 <script setup>
 import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+
 </script>
 
 <template>
@@ -8,14 +12,14 @@ import ApplicationLogo from '@/Components/ApplicationLogo.vue';
             <ApplicationLogo class="block h-12 w-auto" />
 
             <h1 class="mt-8 text-2xl font-medium text-gray-900">
-                Welcome {{ $page.props.auth.user.name }}
+                {{ t('welcome_page.welcome_user') }} {{ $page.props.auth.user.name }}
             </h1>
 
         </div>
 
         <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 p-6 lg:p-8">
             <div>
-                Welcome to your dashboard
+                {{ t('welcome_page.welcome_user_slogan') }}
             </div>
 
         </div>
